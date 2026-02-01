7507d6e9fcc56769ca9c46f41515843dbd268fae
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2026-02-01 13:54:16 -0800

    Figured out how to open a small window within a browser

---
+++ create-task-page/calendar.js
@@ -0,0 +1,1 @@
+//Function to check if leap year. If the year is divisible by 4 it will be a leap year
---
+++ create-task-page/javascript.js
@@ -0,0 +1,5 @@
+//Generate a calendar
+function openNewWindow(){
+    
+}
+//Generate a box that allows you to select time of day from 0-24hrs of that day
