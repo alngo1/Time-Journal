174065d552283c4b3ba29025bbc8d82824599cc5
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2026-02-17 01:39:02 -0800

    Updated calendar class, testing generating a single month for calendar array

--- create-task-page/calendar.js
+++ create-task-page/calendar.js
@@ -32,24 +32,96 @@ let months_leap = {
 
 days_of_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 
+//Classes----------------------------------------------------------------------------------------------------------------------------------------------------------
 class CalendarDate{
     constructor(year, month, month_day, week_day) {
-        this.year = year; //[2025-2027]
-        this.month = month; //[0-11] <-- "January-December"
-        this.month_day = month_day; //[0-31]
-        this.week_day = week_day; //[0-6] <-- "Sunday-Saturday"
+        this._year = year; //[2025-2027]
+        this._month = month; //[0-11] <-- "January-December"
+        this._month_day = month_day; //[0-31]
+        this._week_day = week_day; //[0-6] <-- "Sunday-Saturday"
+    }
+
+    //FIX THIS: Consider Automating a method to generate getters and setters for multiple items rather than manually typing out for each element
+    get year(){
+        return(this._year)
+    }
+    set year(value){
+        return(this._year = value)
+    }
+
+    get month(){
+        return(this._month)
+    }
+    set month(value){
+        return(this._month = value)
+    }
+
+    get month_day(){
+        return(this._month_day)
+    }
+    set month_day(value){
+        return(this._month_day = value)
+    }
+
+    get week_day(){
+        return(this._week_day)
+    }
+    set week_day(value){
+        return(this._week_day = value)
     }
 }
-let epoch = new CalendarDate(2025, 11, 28, 0)  //epoch for 12/28/25, Sunday
 
+//Helper Functions----------------------------------------------------------------------------------------------------------------------------------------------------------
+//Updates new Date() to tomorrow's date
+function update_to_tomorrow(today){
+    let tomorrow = new Date(today)
+    tomorrow.setDate(today.getDate() + 1)
+    return(tomorrow)
+}
+
+//Instantiates CalendarDate object
+function create_current_calendar_day(today){
+    let current_year = today.getFullYear()
+    let current_month = today.getMonth()
+    let current_month_day = today.getDate()
+    let current_week_day = today.getDay()
+    output_today = new CalendarDate(current_year, current_month, current_month_day, current_week_day)
+    return(output_today)
+}
 let today = new Date()
-let year = today.getFullYear()
+let current_year = today.getFullYear()
 let current_month = today.getMonth()
 let current_month_day = today.getDate()
 let current_week_day = today.getDay()
-// year = today.now()
-console.log(today)
-console.log(year)
-console.log(current_month)
-console.log(current_month_day)
-console.log(current_week_day)
+let current_day_hours = today.getHours()
+let current_day_minutes = today.getMinutes()
+let current_day_milliseconds = today.getMilliseconds()
+
+current_day = new CalendarDate(current_year, current_month, current_month_day, current_week_day)
+
+let calendar_epoch = new CalendarDate(2025, 11, 28, 0)  //epoch for 12/28/25, Sunday
+
+
+let epoch = new Date(2025, 11, 28, current_day_hours, current_day_minutes, current_day_milliseconds)
+let starting_epoch = new Date(epoch)
+starting_epoch.setDate(epoch.getDate()-1)
+
+// console.log(starting_epoch)
+let month_january = []
+let newest_day = starting_epoch
+
+for (let i = 0; i < 6; i++){
+    let week = []
+    for (let j = 0; j < 7; j++){
+        let next_day = update_to_tomorrow(newest_day)
+        newest_day = next_day
+        week.push(next_day)
+    }
+    month_january.push(week)
+}
+
+console.log(month_january)
+
+function generate_month_group(){
+    month_array = []
+}
